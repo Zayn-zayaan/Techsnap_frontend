0a01354d5b08ccd32ef3bba5db6cb87c2be6c428
Zain zahid <zaynzahid2000@gmail.com>
2021-10-09 01:37:32 +0530

    Designed sidebar dropdown

---
+++ script.js
@@ -0,0 +1,33 @@
+var basicsbtn = document.querySelector('.dd-icon-basics');
+var basicsdd = document.querySelector('.dd-basics');
+var jobbtn = document.querySelector('.dd-icon-job');
+var jobdd = document.querySelector('.dd-job');
+var skillsbtn = document.querySelector('.dd-icon-skills');
+var skillsdd = document.querySelector('.dd-skills');
+var careerbtn = document.querySelector('.dd-icon-career');
+var careerdd = document.querySelector('.dd-career');
+var gettingjobbtn = document.querySelector('.dd-icon-gettingjob');
+var gettingjobdd = document.querySelector('.dd-gettingjob');
+
+
+basicsbtn.addEventListener("click", ()=>{
+    basicsdd.classList.toggle('dis-basics-open');
+
+})
+
+jobbtn.addEventListener("click", ()=>{
+    jobdd.classList.toggle('dis-jobs-open');
+
+})
+skillsbtn.addEventListener("click", ()=>{
+    skillsdd.classList.toggle('dis-skills-open');
+
+})
+careerbtn.addEventListener("click", ()=>{
+    careerdd.classList.toggle('dis-career-open');
+
+})
+gettingjobbtn.addEventListener("click", ()=>{
+    gettingjobdd.classList.toggle('dis-gettingjob-open');
+
+})
